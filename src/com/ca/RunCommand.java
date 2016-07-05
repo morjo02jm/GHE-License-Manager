e31452530b6dec6d70c0a6f5c6f58219b1209165
gaoyu01 <gaoyu01@ca.com>
2016-07-05 15:50:22 -0400

    fix throwable

--- src/com/ca/RunCommand.java
+++ src/com/ca/RunCommand.java
@@ -1,34 +1,32 @@
 package com.ca;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.util.logging.Level;
+import java.util.logging.Logger; 
  
 public class RunCommand {
-    public static String execute(String cmd) {
+    private static final Logger LOGGER = Logger.getLogger(RunCommand.class.getName() );
+	public static String execute(String cmd) {
 		String output = null;
- 
         try {
 			String line;
 			StringBuffer buffer = new StringBuffer();
- 
             Runtime rt = Runtime.getRuntime();
             Process process = rt.exec(cmd);
- 
-	    InputStreamReader isr = new InputStreamReader( process.getInputStream() );
+	        InputStreamReader isr = new InputStreamReader( process.getInputStream() );
             BufferedReader br = new BufferedReader( isr );
  
             while( ( line = br.readLine() ) != null ) {
-			/*	buffer.append( "<br>" ); */
 				buffer.append( line );
 	   		}
- 
             int exitValue = process.waitFor();
             System.out.println( "ExitValue: " + exitValue );
- 
             output = buffer.toString();
-        } catch (Throwable t) {
-        	t.printStackTrace();
+        } catch (Exception e) {
+        	/*e.printStackTrace();*/
+			LOGGER.log(Level.SEVERE, e.toString(), e);
         }
         return output;
     }
-
 }
+
